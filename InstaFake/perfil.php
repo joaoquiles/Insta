fb88935712c771d6648faa45c8e0bc736b0f526f
Vinicius Passoni <viniciuspassoni97@gmail.com>
2019-06-02 21:33:28 -0300

    Perfil 1.1

---
+++ InstaFake/perfil.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Perfil Pessoal</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style-perfil.css">
+</head>
+<body>
+	<div id="div1">
+		<h1>Instagram</h1>
+		<input type="text" name="busca" placeholder="Buscar">
+	</div>
+	<div>
+		
+	</div>
+	<div>
+		
+	</div>
+
+</body>
+</html>
