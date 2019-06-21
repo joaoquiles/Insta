dbf7be5cd01aec9859f20078c5adadd4af996dbf
Vinicius Passoni <viniciuspassoni97@gmail.com>
2019-06-21 01:21:21 -0300

    Perfil style

--- InstaFake/perfil.php
+++ InstaFake/perfil.php
@@ -7,14 +7,20 @@
 </head>
 <body>
 	<div id="div1">
+		<img src="https://img.icons8.com/ios/48/000000/instagram-new.png" id="img1">
 		<form method="post" action="buscarUsuarios.php">
-			<h1>Instagram</h1>
+			<h1>InstaFake</h1>
 			<input type="text" name="busca" placeholder="Buscar">
 			<input type="submit" name="botao" value="Buscar">
 		</form>
+		<img src="https://img.icons8.com/ios/64/000000/add-user-male.png" id="img3">
+		<img src="https://img.icons8.com/carbon-copy/64/000000/undo.png" id="img2" >
 	</div>
 	<div id="div2">
-		<h2>Informaçoes do perfil</h2>
+		<img src="https://img.icons8.com/ios/100/000000/name.png" id="img4">
+		<h2>Nome do Usuario</h2>
+		<button class="btn btn-blue">Seguidores</button>
+		<button class="btn btn-blue btn-blue-position">Seguindo</button>
 	</div>
 	<div id="div3">
     <h2>Fotos</h2>
